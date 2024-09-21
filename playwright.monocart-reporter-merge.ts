b9aa925f22878d73fec5da796847c84b887a30be
Eduardo Serrano <eduardomserrano@outlook.com>
2024-09-21 17:50:01 +0100

    add working example of TS file

--- playwright.monocart-reporter-merge.ts
+++ playwright.monocart-reporter-merge.ts
@@ -1,6 +1,18 @@
-
 import { getMonocartReporterOptions } from "playwright.monocart-reporter-options";
 import { reportersOutputDir } from "playwright.shared-vars";
 
 const options = getMonocartReporterOptions(reportersOutputDir);
 export default options;
+
+// If I comment the above and uncomment below it works. What doesn't seem to work
+// is importing from other files in the project...
+//
+// import type { MonocartReporterOptions } from "monocart-reporter";
+// import path from "node:path";
+// const reportersOutputDir = path.resolve("./test-reporters/playwright");
+// const monocartReporterOutputDir = path.resolve(reportersOutputDir, "monocart-reporter");
+// const options: MonocartReporterOptions = {
+//   name: "Playground app for @ln/ngx-module-federation-tools",
+//   outputFile: path.resolve(monocartReporterOutputDir, "monocart-report.html"),
+// }
+// export default options;
